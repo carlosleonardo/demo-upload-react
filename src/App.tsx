aa1d0f48f7117a8c33938e07bf3d443a18eace07
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-10-03 13:47:38 -0300

    Limpeza de css desnecessário e tela inicial de upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
+import { ChangeEvent, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [urlImage, setUrlImage] = useState("");
+
+    function aoMudarArquivo(event: ChangeEvent<HTMLInputElement>): void {
+        const arquivos = event.target.files;
+        if (arquivos) {
+            const url = URL.createObjectURL(arquivos[0]);
+            setUrlImage(url);
+        }
+    }
+
     return (
         <div className="container">
-            <h3>Demo de Upload</h3>
+            <form className="row">
+                <h3>Demo de Upload</h3>
+                <label htmlFor="arquivos">Selecione uma imagem</label>
+                <input
+                    type="file"
+                    id="arquivos"
+                    className="form-control"
+                    onChange={aoMudarArquivo}
+                />
+                <button type="submit" className="btn btn-primary mt-3">
+                    Enviar arquivos
+                </button>
+                {urlImage !== "" && (
+                    <img
+                        src={urlImage}
+                        alt="imagem"
+                        style={{ maxWidth: "480px", height: "auto" }}
+                    />
+                )}
+            </form>
         </div>
     );
 }
-
 export default App;
